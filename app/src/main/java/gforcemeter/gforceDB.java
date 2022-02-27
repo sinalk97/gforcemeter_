a3b1fd1310eef265ea339cc5ff30a1a9d0d8cebe
Sinan <sinalk@freenet.de>
2022-02-28 00:37:10 +0100

    added ormlite-android-6.1.jar to have the most current version of orm lite :) still i dont get how it works :(

--- app/src/main/java/gforcemeter/gforceDB.java
+++ app/src/main/java/gforcemeter/gforceDB.java
@@ -1,8 +1,18 @@
 package gforcemeter;
 
 
-import com.j256.ormlite.field.*;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
 
 public class gforceDB {
+    private
+    //to save:
+    double minVal;
+    double maxVal;
 
+    protected void onDestroy(){
+
+    }
 }
